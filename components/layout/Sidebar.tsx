452b2cbce9d8d56ed0af0f95e2c9f26ab7659ea3
Pratik Goyal <pratikgoyal2712@gmail.com>
2026-03-06 01:29:33 +0530

    finished layout w slider shadcn compoenent

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import { Slider } from "@/components/ui/slider";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [padding, setPadding] = useState<number[]>([30]);
+  const [rounding, setRounding] = useState<number[]>([30]);
+
+  console.log(padding, rounding);
+
   return (
-    <div className="w-[350px] border-r border-gray-200 p-4 flex flex-col gap-6">
+    <div className="w-[350px] border-r border-gray-200 p-4 flex flex-col gap-6 pb-[150px]">
       <h2 className="text-lg font-semibold">Transcript</h2>
 
       <div className="flex-1 overflow-y-auto">Transcript will go here</div>
@@ -11,20 +19,24 @@ export default function Sidebar() {
         <div>
           <label className="text-sm">Padding</label>
           <Slider
-            defaultValue={[75]}
+            defaultValue={[30]}
             max={100}
             step={1}
-            className="mx-auto w-full max-w-xs"
+            className="mx-auto py-3 w-full max-w-xs"
+            value={padding}
+            onValueChange={setPadding}
           />
         </div>
 
         <div>
           <label className="text-sm">Rounding</label>
           <Slider
-            defaultValue={[75]}
+            defaultValue={[30]}
             max={100}
             step={1}
-            className="mx-auto w-full max-w-xs"
+            className="mx-auto py-3 w-full max-w-xs"
+            value={rounding}
+            onValueChange={setRounding}
           />
         </div>
       </div>
--- components/player/Timeline.tsx
+++ components/player/Timeline.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { Slider } from "../ui/slider";
+import { useState } from "react";
+
 export default function Timeline() {
+  const [value, setValue] = useState<number[]>([0]);
+
   return (
     <div className="w-[700px] flex items-center gap-4">
       <button className="px-3 py-1 bg-black text-white rounded">Play</button>
 
-      <input type="range" className="flex-1" />
+      <Slider
+        defaultValue={[0]}
+        max={100}
+        step={1}
+        className="flex-1"
+        value={value}
+        onValueChange={setValue}
+      />
     </div>
   );
 }
